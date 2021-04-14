2071a7a65ef9d8c67fb601d1edfd7a27239e5470
MomeNoob <bouabidi.mohamed@ofppt-edu.ma>
2021-04-14 19:14:36 +0000

    menu principale
    menu principale

---
+++ Gestion De Stock/Forms/MenuPrincipale.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace Gestion_De_Stock.Forms
+{
+    partial class MenuPrincipale
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MenuPrincipale));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button_produit = new System.Windows.Forms.Button();
+            this.button_fournisseur = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button_categories = new System.Windows.Forms.Button();
+            this.button_historique = new System.Windows.Forms.Button();
+            this.button_utilisateur = new System.Windows.Forms.Button();
+            this.App_Logo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(39)))), ((int)(((byte)(40)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.App_Logo);
+            this.panel1.Controls.Add(this.button_utilisateur);
+            this.panel1.Controls.Add(this.button_historique);
+            this.panel1.Controls.Add(this.button_categories);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button_fournisseur);
+            this.panel1.Controls.Add(this.button_produit);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(20, 30);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(278, 960);
+            this.panel1.TabIndex = 0;
+            // 
+            // button_produit
+            // 
+            this.button_produit.BackColor = System.Drawing.Color.DarkRed;
+            this.button_produit.FlatAppearance.BorderSize = 0;
+            this.button_produit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_produit.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_produit.ForeColor = System.Drawing.Color.LightGray;
+            this.button_produit.Location = new System.Drawing.Point(0, 220);
+            this.button_produit.Name = "button_produit";
+            this.button_produit.Size = new System.Drawing.Size(278, 67);
+            this.button_produit.TabIndex = 1;
+            this.button_produit.Text = "PRODUIT";
+            this.button_produit.UseVisualStyleBackColor = false;
+            this.button_produit.Click += new System.EventHandler(this.button_produit_Click);
+            // 
+            // button_fournisseur
+            // 
+            this.button_fournisseur.BackColor = System.Drawing.Color.DarkRed;
+            this.button_fournisseur.FlatAppearance.BorderSize = 0;
+            this.button_fournisseur.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_fournisseur.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_fournisseur.ForeColor = System.Drawing.Color.LightGray;
+            this.button_fournisseur.Location = new System.Drawing.Point(0, 348);
+            this.button_fournisseur.Name = "button_fournisseur";
+            this.button_fournisseur.Size = new System.Drawing.Size(278, 67);
+            this.button_fournisseur.TabIndex = 1;
+            this.button_fournisseur.Text = "FOURNISSEUR";
+            this.button_fournisseur.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DarkRed;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.LightGray;
+            this.button3.Location = new System.Drawing.Point(0, 476);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(278, 67);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "//cmd fournisseur";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button_categories
+            // 
+            this.button_categories.BackColor = System.Drawing.Color.DarkRed;
+            this.button_categories.FlatAppearance.BorderSize = 0;
+            this.button_categories.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_categories.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_categories.ForeColor = System.Drawing.Color.LightGray;
+            this.button_categories.Location = new System.Drawing.Point(0, 604);
+            this.button_categories.Name = "button_categories";
+            this.button_categories.Size = new System.Drawing.Size(278, 67);
+            this.button_categories.TabIndex = 1;
+            this.button_categories.Text = "CATEGORIES";
+            this.button_categories.UseVisualStyleBackColor = false;
+            // 
+            // button_historique
+            // 
+            this.button_historique.BackColor = System.Drawing.Color.DarkRed;
+            this.button_historique.FlatAppearance.BorderSize = 0;
+            this.button_historique.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_historique.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_historique.ForeColor = System.Drawing.Color.LightGray;
+            this.button_historique.Location = new System.Drawing.Point(0, 732);
+            this.button_historique.Name = "button_historique";
+            this.button_historique.Size = new System.Drawing.Size(278, 67);
+            this.button_historique.TabIndex = 1;
+            this.button_historique.Text = "HISTORIQUE";
+            this.button_historique.UseVisualStyleBackColor = false;
+            // 
+            // button_utilisateur
+            // 
+            this.button_utilisateur.BackColor = System.Drawing.Color.DarkRed;
+            this.button_utilisateur.FlatAppearance.BorderSize = 0;
+            this.button_utilisateur.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_utilisateur.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_utilisateur.ForeColor = System.Drawing.Color.LightGray;
+            this.button_utilisateur.Location = new System.Drawing.Point(0, 860);
+            this.button_utilisateur.Name = "button_utilisateur";
+            this.button_utilisateur.Size = new System.Drawing.Size(278, 67);
+            this.button_utilisateur.TabIndex = 1;
+            this.button_utilisateur.Text = "UTILISATEUR";
+            this.button_utilisateur.UseVisualStyleBackColor = false;
+            // 
+            // App_Logo
+            // 
+            this.App_Logo.FlatAppearance.BorderSize = 0;
+            this.App_Logo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.App_Logo.Image = global::Gestion_De_Stock.Properties.Resources._172150;
+            this.App_Logo.Location = new System.Drawing.Point(15, 17);
+            this.App_Logo.Name = "App_Logo";
+            this.App_Logo.Size = new System.Drawing.Size(63, 65);
+            this.App_Logo.TabIndex = 1;
+            this.App_Logo.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.LightGray;
+            this.label1.Location = new System.Drawing.Point(84, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Gestion de Stock";
+            // 
+            // MenuPrincipale
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1501, 1010);
+            this.Controls.Add(this.panel1);
+            this.DisplayHeader = false;
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Movable = false;
+            this.Name = "MenuPrincipale";
+            this.Padding = new System.Windows.Forms.Padding(20, 30, 20, 20);
+            this.Resizable = false;
+            this.Text = "Gestion de Stock";
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.MenuPrincipale_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button_utilisateur;
+        private System.Windows.Forms.Button button_historique;
+        private System.Windows.Forms.Button button_categories;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button_fournisseur;
+        private System.Windows.Forms.Button button_produit;
+        private System.Windows.Forms.Button App_Logo;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Gestion De Stock/Forms/MenuPrincipale.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gestion_De_Stock.Forms
+{
+    public partial class MenuPrincipale : MetroFramework.Forms.MetroForm//Form
+    {
+        public MenuPrincipale()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuPrincipale_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        private void button_produit_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        //TODO: Change buttons color on click..
+        private void buttonsColorChanger()
+        {
+
+        }
+    }
+}
