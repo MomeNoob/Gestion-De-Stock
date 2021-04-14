90515a0004435c18fa74360dd0742ce6f89e8168
Sorakannie <amomazegh1999@gmail.com>
2021-04-14 12:44:25 +0000

    Added User Class

---
+++ Gestion De Stock/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+enum UserType
+{
+    Admin,
+    NormalUser
+}
+
+namespace Gestion_De_Stock
+{
+    class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public UserType UserType { get; set; }
+
+        public User(string userName, string password,UserType userType)
+        {
+            UserName = userName;
+            Password = password;
+            UserType = userType;
+        }
+
+        //Checks if the user exists
+        public bool IsValidUser(User user)
+        {
+            //TODO : checks the credentials against the database
+            return true;
+        }
+    }
+}
