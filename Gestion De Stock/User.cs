faa9ecd974982b6bb097ae1b5f329010d456ced5
Sorakannie <amomazegh1999@gmail.com>
2021-04-14 13:07:59 +0000

    Added Methods to User class

--- Gestion De Stock/User.cs
+++ Gestion De Stock/User.cs
@@ -32,5 +32,23 @@ namespace Gestion_De_Stock
             //TODO : checks the credentials against the database
             return true;
         }
+
+        public void AddNewUser(User user)
+        {
+            //TODO : add a new user to the database
+        }
+
+        public void DeleteUser(User user)
+        {
+            //TODO : delete user from database
+        }
+
+        public void ModifyUser(User user, User newDetails)
+        {
+            user.UserType = newDetails.UserType;
+            user.UserName = newDetails.UserName;
+            user.Password = newDetails.Password;
+            //TODO : apply changes to the database
+        }
     }
 }
